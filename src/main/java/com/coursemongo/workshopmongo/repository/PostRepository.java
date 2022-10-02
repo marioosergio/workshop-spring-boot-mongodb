cf4db70f19e20e4cb3e876e0ee3732c02d4f379b
Mario Sergio <msergioc@gmail.com>
2022-10-02 19:12:09 -0300

    Entity Post com User aninhado

---
+++ src/main/java/com/coursemongo/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.coursemongo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coursemongo.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
